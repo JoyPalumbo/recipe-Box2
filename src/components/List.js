185d1b971a8da1cc96615542efc75e9513d3567d
Joy <greeniejoy@gmail.com>
2019-10-11 10:03:17 -0500

    added list component

---
+++ src/components/List.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+//This will be where the list of recipes are that are saved and people can select
+class List extends React.Component {
+  constructor(props) {
+    super(props);
+    //     this.state = {
+
+  }
+}
+
+
+export default List;
